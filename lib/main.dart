458de183bd9803115e849993f1729ac57aa8a9bf
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-03 16:54:29 +0100

    Added a background color

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     MaterialApp(
         home: Scaffold(
+      backgroundColor: Colors.grey,
       appBar: AppBar(
         backgroundColor: Colors.black87,
         title: Text('I Am Poor'),
